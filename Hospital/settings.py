ee6dbb22401ee5cc3d3740373853976d2f9e0dc3
Evgeniy Tretyak <Evgeniy@iMac-Yana.local>
2023-01-19 19:12:47 +0200

    add requirements.txt and updated settings

--- Hospital/settings.py
+++ Hospital/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 import os.path
 from pathlib import Path
 
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3(okllhdut^bo7*#)u_f4e&2ddjt7q0r5f406$$uqo7g4igx%^"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-3(okllhdut^bo7*#)u_f4e&2ddjt7q0r5f406$$uqo7g4igx%^")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -104,6 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = "app.Doctor"
+
+LOGIN_REDIRECT_URL = "/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
