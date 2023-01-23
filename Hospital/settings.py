983db23334c29aa7ed8273e83c76c1cb9e6bca17
Evgeniy <genoktretiack@gmail.com>
2023-01-23 13:15:25 +0200

    Remove insecure Django secret key from settings

--- Hospital/settings.py
+++ Hospital/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-3(okllhdut^bo7*#)u_f4e&2ddjt7q0r5f406$$uqo7g4igx%^")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
